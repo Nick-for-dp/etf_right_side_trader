8417acedd9c9eeba32ea0bb22fa1930c82abe88d
wukehan <kehanwu1997@163.com>
2026-06-07 15:41:49 +0800

    --feat baseline v2.4: 1、基于至多持有90个交易日的交易规则制定因子。 2、基于70/15/15阶梯建仓策略 3、基于分市场/分波动配置止损策略。

--- src/backtest/__init__.py
+++
@@ -1,7 +0,0 @@
-from .odds_gate_backtest import (
-    OddsGateBacktest,
-    run_odds_gate_backtest,
-    format_comparison_report,
-)
-
-__all__ = ["OddsGateBacktest", "run_odds_gate_backtest", "format_comparison_report"]
